ed09e047d849008ffe548ee47179637c4903415e
Monir <mrmonir0558@gmail.com>
2024-12-16 00:41:34 +0600

    UpdateJob and myPosted page almost done

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
 
 const JobCard = ({ job }) => {
     const { title, deadline, category, min_price, max_price, description, bid_count } = job || {};
@@ -8,7 +9,7 @@ const JobCard = ({ job }) => {
     return (
         <Link to={`/job/1`} className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all">
             <div className="flex items-center justify-between">
-                <span className="text-xs font-light text-gray-800 ">Deadline: {deadline}</span>
+                <span className="text-xs font-light text-gray-800 ">Deadline: {format(new Date(deadline), 'P')}</span>
                 <span className="px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full ">{category}</span>
             </div>
 
