bbadd378cde6128fdc6d9d4cb3e1d351734c93e1
Monir <mrmonir0558@gmail.com>
2024-12-16 01:08:27 +0600

    My posted jobs updated successfully done

--- src/pages/UpdateJob.jsx
+++ src/pages/UpdateJob.jsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { useParams } from 'react-router-dom';
+import {useNavigate, useParams } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
+import toast from 'react-hot-toast';
 
 const UpdateJob = () => {
+  const navigate = useNavigate();
   const {user} = useContext(AuthContext)
     const { id } = useParams();
     const [startDate, setStartDate] = useState(new Date());
@@ -21,14 +23,53 @@ const UpdateJob = () => {
       setJob(data);
       setStartDate(new Date(data.deadline));
     };
-    console.log(job);
+    
+   const handleSubmit = async e => {
+       e.preventDefault();
+       const form = e.target;
+       const title = form.job_title.value;
+       const email = form.email.value;
+       const deadline = startDate;
+       const category = form.category.value;
+       const min_price = parseFloat(form.min_price.value);
+       const max_price = parseFloat(form.max_price.value);
+       const description = form.description.value;
+
+       const formData = {
+           title,
+           buyer: {
+               email,
+               name: user?.displayName,
+               photo: user?.photoURL
+           },
+           deadline,
+           category,
+           min_price,
+           max_price,
+           description,
+           bid_count: job.bid_count
+       };
+
+       try {
+           // make a post request
+           await axios.put(`${import.meta.env.VITE_API_URL}/update-job/${id}`, formData);
+           // reset form
+           form.reset();
+           // show toast
+           toast.success('Data update Successfully !!!');
+           // navigate
+           navigate('/my-posted-jobs');
+       } catch (err) {
+           toast.error(err.message);
+       }
+   };
 
     return (
         <div className="flex justify-center items-center min-h-[calc(100vh-306px)] my-12">
             <section className=" p-2 md:p-6 mx-auto bg-white rounded-md shadow-md ">
                 <h2 className="text-lg font-semibold text-gray-700 capitalize ">Update a Job</h2>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                         <div>
                             <label className="text-gray-700 " htmlFor="job_title">
