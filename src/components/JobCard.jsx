63dc284a7abf5914c422bf4f3915022ebcae1a32
Monir <mrmonir0558@gmail.com>
2024-12-16 08:50:40 +0600

    Job details page almost done

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
 
 const JobCard = ({ job }) => {
-    const { title, deadline, category, min_price, max_price, description, bid_count } = job || {};
+    const { _id, title, deadline, category, min_price, max_price, description, bid_count } = job || {};
 
     return (
-        <Link to={`/job/1`} className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all">
+        <Link to={`/job/${_id}`} className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all">
             <div className="flex items-center justify-between">
                 <span className="text-xs font-light text-gray-800 ">Deadline: {format(new Date(deadline), 'P')}</span>
                 <span className="px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full ">{category}</span>
